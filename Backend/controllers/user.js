1ade82354d7ebede360b99ff1e6eb626481bffc9
Sanket <sanketsabale9767@gmail.com>
2022-06-07 09:27:12 +0530

    Parameter and User Controllers are done

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -38,5 +39,52 @@ exports.updateUser = (req,res) => {
       return res.json(user)
     }
     );
+
+}
+
+exports.userPurchaseList = (req,res) => {
+    Order.find({user : req.profile._id})
+    .populate("user","_id name")
+    .exec((err,order) => {
+      if(err){
+        return res.status(400).json({
+          error : "No Order in this account"
+        })
+      }
+      return res.json(order)
+    })
+}
+
+exports.pushOrderInPurchaseList = (req,res,next) =>{
+  
+  let purchases = [];
+
+  req.body.order.products.forEach(product => {
+    purchases.push({
+      _id:product._id,
+      name:product.name,
+      description : product.description,
+      category:product.category,
+      quantity:product.quantity,
+      amount : req.body.order.amount,
+      transaction_id : req.body.order.transaction_id
+    })
+  });
+
+  //store these in db
+  User.findOneAndUpdate(
+    {_id:req.profile._id},
+    {$push: {purchases:purchases}},
+    {new:true},
+    (err,purchases) =>{
+      if(err){
+        return res.status(400).json({
+          error :"Unable To save Purchase List"
+        })
+      }
+      
+  next();
+    }
+    )
 
 }
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {isAdmin,isAuthenticated,isSignedIn} = require("../controllers/auth");
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const { getUserById, getUser, updateUser,userPurchaseList} = require("../controllers/user");
 
 router.param("userId",getUserById);
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put('/user/:userId',isSignedIn,isAuthenticated,updateUser);
+router.get("/orders/user/:userId",isSignedIn,isAuthenticated,userPurchaseList);
 module.exports = router;
 
