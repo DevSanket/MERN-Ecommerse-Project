301e5d24bec848c09f53404210e54090f0ab9175
Sanket <sanketsabale9767@gmail.com>
2022-06-09 11:30:11 +0530

    updated Category is Done

---
+++ Backend/controllers/category.js
@@ -0,0 +1,61 @@
+const Category = require('../models/category');
+
+exports.getCategoryById = (req,res,next,id) =>{
+    
+    Category.findById(id).exec((err,cate) => {
+        if(err){
+            return res.status(400).json({
+                error:"Category not found in DB"
+            })
+        }
+
+        req.category = cate;
+        next();
+    })
+    
+    
+};
+
+exports.createCategory = (req,res) => {
+    const category = new Category(req.body);
+    category.save((err,category) => {
+        if(err){
+            return res.status(400).json({
+                error:"Not able to save category in DB"
+            });
+        }
+        res.json({category});
+    })
+}
+
+exports.getCategory = (req,res) => {
+    return res.json(req.category);
+}
+
+exports.getAllCategories = (req,res) => {
+    Category.find().exec((err,items) => {
+        if(err){
+            return res.status(400).json({
+                error : "No category was found"
+            });
+        }
+        res.json(items);
+    });
+};
+
+
+exports.updateCategory =  (req,res) => {
+    const category = req.category;
+    category.name = req.body.name;
+
+    category.save((err,updatedCategory) => {
+        if(err){
+            return res.status(400).json({
+                error:"Error while update Category"
+            })
+
+        }
+
+        res.json(updatedCategory);
+    })
+}
--- Backend/routes/category.js
+++ Backend/routes/category.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {isSignedIn,isAdmin,isAuthenticated} = require('../controllers/auth');
+const { getCategoryById, createCategory, getCategory, getAllCategories, updateCategory } = require('../controllers/category');
+const {getUserById} = require('../controllers/user');
 
+//params
+router.param("userId",getUserById);
+router.param("categoryId",getCategoryById);
+
+//actual routes goes here
+router.post("/category",isSignedIn,isAuthenticated,isAdmin,createCategory);
+
+//Read Route
+router.get("/category/:categoryId",getCategory);
+router.get("/categories",getAllCategories);
+
+//Updated Routes
+router.put("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,updateCategory);
+
+exports.module = router;
